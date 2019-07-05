907f438b28bc20fcf8e68131ccb3f08285f5ecbb
Glitch (pwa01) <none>
2019-07-05 14:46:15 +0000

    first copy

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
+// Redirect HTTP to HTTPS,
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
